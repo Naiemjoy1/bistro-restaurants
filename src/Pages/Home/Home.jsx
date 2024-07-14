88a038246929bcb52dfd08ef5bf9df7e73ef643e
Naiem Hasan <naiemjoy1@gmail.com>
2024-07-14 23:00:14 +0600

    banner added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "./Banner/Banner";
+
 const Home = () => {
   return (
-    <div>
-      <p>this is home</p>
+    <div className="container mx-auto">
+      <Banner></Banner>
     </div>
   );
 };
