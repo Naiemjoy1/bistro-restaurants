acaeb6ed53137c79aae4ec91f8f831e91c593a9a
Naiem Hasan <naiemjoy1@gmail.com>
2024-07-15 12:20:35 +0600

    oder online added and section title make and porps type off

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
+import OrderOnline from "./OrderOnline/OrderOnline";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <OrderOnline></OrderOnline>
     </div>
   );
 };
