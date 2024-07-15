54e4f1980fa4223abf7c774295e7d47387bbe6da
Naiem Hasan <naiemjoy1@gmail.com>
2024-07-15 12:37:49 +0600

    menu items added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner/Banner";
 import OrderOnline from "./OrderOnline/OrderOnline";
+import PopularMenu from "./PopularMenu/PopularMenu";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <OrderOnline></OrderOnline>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
