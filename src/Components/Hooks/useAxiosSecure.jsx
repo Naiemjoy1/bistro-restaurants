1eeeb07f42a49bd0bc48a34f5f9f258e55a3fede
Naiem Hasan <naiemjoy1@gmail.com>
2024-07-31 01:02:58 +0600

    jwt and verify token

--- src/Components/Hooks/useAxiosSecure.jsx
+++ src/Components/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,47 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("Request Token:", token);
+      if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+      } else {
+        console.warn("No token found in localStorage.");
+      }
+      return config;
+    },
+    function (error) {
+      console.error("Request error:", error);
+      return Promise.reject(error);
+    }
+  );
+
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response ? error.response.status : null;
+      console.log("Response error status:", status);
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
--- src/Components/Hooks/useUsers.jsx
+++ src/Components/Hooks/useUsers.jsx
@@ -7,7 +7,11 @@ const useUsers = () => {
   const { refetch, data: users = [] } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
-      const res = await axiosSecure.get("/users");
+      const res = await axiosSecure.get("/users", {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("access-token")}`,
+        },
+      });
       return res.data;
     },
   });
